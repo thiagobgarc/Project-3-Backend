4fa1f3fb22ede4f4c950d3e18ce5867f1cdaf020
Thiago Bueno Garcia <Thiagobuenogarcia1@gmail.com>
2023-03-12 18:44:50 -0400

    backbone for backend

---
+++ server.js
@@ -0,0 +1,48 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+require("dotenv").config()
+// pull PORT from .env, give default value of 3000
+const { PORT = 3000 } = process.env
+// import express
+const express = require("express")
+// create application object
+const app = express()
+//import mongoose
+const mongoose = require("mongoose")
+// import middleware
+const cors = require("cors")
+const morgan = require("morgan")
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// mongoose.connect(PORT, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   })
+
+//   mongoose.connection.on("open", () => console.log("Youre connected to mongoose"))
+//   .on("close", () => console.log("You are disconnected"))
+//   .on("error", (error) => console.log(error))
+
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req,res) => {
+    res.send("hello world")
+})
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
