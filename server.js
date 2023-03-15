fee49f1d73645d9a4aa3b067e281946614533fd2
slaul <sameer.laul@uconn.edu>
2023-03-15 19:39:44 -0400

    server.js - roommate seed data added

--- server.js
+++ server.js
@@ -58,17 +58,107 @@ const ApartmentSchema = new mongoose.Schema({
 
  // Roommate Schema
   const roommateSchema = new mongoose.Schema({
-    Name: String,
+    name: String,
     age: Number,
     gender: String,
-    housingStyleWanted: String,
+    housingStyleWanted: Array,
     smoker: Boolean,
     earlyRiser: Boolean,
     major: String,
-    interests: String,
+    interests: Array,
   })
 const Roommate = mongoose.model("Roommate", roommateSchema)
 
+///////////////////////////////
+// SEED DATA
+//////////////////////////////
+// Apartment Seed
+
+
+// Roommate Seed
+Roommate.create ([
+    {
+       name: 'John Doe',
+       age: 19,
+       gender: 'male',
+       housingStyleWanted: ['dorm'],
+       smoker: false,
+       earlyRiser: true,
+       major: 'biology',
+       interests: ['sports', 'movies', 'hiking'],
+    },
+    {
+       name: 'Elizabeth Walker',
+       age: 18,
+       gender: 'female',
+       housingStyleWanted: ['dorm'],
+       smoker: false,
+       earlyRiser: false,
+       major: 'economics',
+       interests: ['fashion', 'working out', 'music'],
+    },
+    {
+       name: 'Priya Jain',
+       age: 20,
+       gender: 'female',
+       housingStyleWanted: ['apartment', 'house'],
+       smoker: false,
+       earlyRiser: true,
+       major: 'computer science',
+       interests: ['dancing', 'reading', 'sports'],
+    },
+    {
+       name: 'Kyrie Jackson',
+       age: 21,
+       gender: 'male',
+       housingStyleWanted: ['apartment'],
+       smoker: false,
+       earlyRiser: true,
+       major: 'physics',
+       interests: ['working out', 'basketball', 'music'],
+    },
+    {
+       name: 'Layla Abbas',
+       age: 18,
+       gender: 'female',
+       housingStyleWanted: ['dorm'],
+       smoker: false,
+       earlyRiser: false,
+       major: 'english',
+       interests: ['writing', 'reading', 'baking'],
+    },
+    {
+       name: 'Jun Park',
+       age: 22,
+       gender: 'male',
+       housingStyleWanted: ['house'],
+       smoker: true,
+       earlyRiser: true,
+       major: 'economics',
+       interests: ['music', 'traveling', 'fashion'],
+    },
+    {
+       name: 'Sasha Smith',
+       age: 21,
+       gender: 'female',
+       housingStyleWanted: ['apartment', 'house'],
+       smoker: false,
+       earlyRiser: true,
+       major: 'gender studies',
+       interests: ['sports', 'reading', 'working out'],
+    },
+    {
+       name: 'Allie Jones',
+       age: 19,
+       gender: 'female',
+       housingStyleWanted: 'dorm',
+       smoker: true,
+       earlyRiser: false,
+       major: 'mathematics',
+       interests: ['movies', 'baking', 'dancing'],
+    }
+ ]) 
+console.log(Roommate)
 ///////////////////////////////
 // MIDDLEWARE
 ////////////////////////////////
