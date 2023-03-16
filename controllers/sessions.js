2de024f58d0f1caf2762119ffe20f491c6392142
thiagobgarc <thiagobuenogarcia1@gmail.com>
2023-03-15 20:31:41 -0400

    Added - npm start

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,16 +1,16 @@
-// IMPORT BCRYPT
-const bcrypt = require('bycrpt')
-// IMPORT EXPRESS
-const express = require('express')
-// IMPORT EXPRESS.ROUTER
-const Sessions = express.Router()
-// IMPORT USERS
-// const User = require('../models/users.js')
+// // IMPORT BCRYPT
+// const bcrypt = require('bycrpt')
+// // IMPORT EXPRESS
+// const express = require('express')
+// // IMPORT EXPRESS.ROUTER
+// const Sessions = express.Router()
+// // IMPORT USERS
+// // const User = require('../models/users.js')
 
-sessions.get('/login', async (req,res) => {
-    // this will held in state in REACT
-    res.json()
-})
+// sessions.get('/login', async (req,res) => {
+//     // this will held in state in REACT
+//     res.json()
+// })
 
-// EXPORT SESSIONS
-module.exports = Sessions
+// // EXPORT SESSIONS
+// module.exports = Sessions
