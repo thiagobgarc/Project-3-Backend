009524d374dfddc12e8cd31264ac917436eb94e1
thiagobgarc <thiagobuenogarcia1@gmail.com>
2023-03-20 21:26:30 -0400

    Changes - Backend - server.js, users.js. utils.js

---
+++ utils/bcrypt-middleware.js
@@ -0,0 +1,11 @@
+const isAuthenticated = (req, res, next) => {
+    // if (req.session.currentUser){
+    //     return next()
+    // } else {
+    //     try {
+    //         res.json(await User.create({}))
+    //     } catch {
+    //         res.status(400).json(error)
+    //     }
+    // }
+}
